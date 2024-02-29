cb9bbf7acc3ae9e1ff7db5373b400e1e9a1089a2
Munoru101 <dennismunoru@gmail.com>
2024-02-29 19:13:07 +0300

    Included the func to meausure height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,6 +13,25 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height, right_height;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	left_height = (int)binary_tree_height(tree->left);
+	right_height = (int)binary_tree_height(tree->right);
+	return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: height of the tree. If tree is NULL return 0.
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
 	size_t left_height, right_height;
 
 	if (tree == NULL)
@@ -21,5 +40,5 @@ int binary_tree_balance(const binary_tree_t *tree)
 	}
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	return ((int)(left_height - right_height));
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
